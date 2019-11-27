d47f47ad88d4b7acebcaf2b1843067b9de095fe0
Benjamin Langley <blangley@mitre.org>
2019-11-27 14:20:45 -0500

    Remove old graph-layout tests before moved to class

--- src/engine/__tests__/graph-layout.test.ts
+++ src/engine/__tests__/graph-layout.test.ts
@@ -1,4 +1,3 @@
-import { graphLayout, produceCoordinates } from '../graph-layout';
 import { Graph } from '../graph';
 
 import samplePathway from '../../__tests__/fixtures/pathways/sample_pathway.json';
@@ -9,13 +8,11 @@ describe('pathway graph layout', () => {
     let graph = new Graph(samplePathway);
     let graphCoordinates = graph.layout();
     console.log(graphCoordinates);
-    const algoOutput = graphLayout(samplePathway);
   });
 
   it('pathway layout set correctly', () => {
     let graph = new Graph(upennPathway);
     let graphCoordinates = graph.layout();
     console.log(graphCoordinates);
-    const algoOutput = graphLayout(upennPathway);
   });
 });
