76220c0445618f287ee584b0e4217bd957af13c9
Dan Lee <daniellee@mitre.org>
2019-12-09 22:04:31 -0500

    Added tests for Node

---
+++ src/components/Node/__tests__/Node.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Node from '../Node';
+
+describe('<Node />', () => {
+  it('renders a node with text, icon, and correct styles', () => {
+    const { container, getByText, getByRole } = render(
+      <Node
+        icon="test"
+        text="Start"
+        isOnPatientPath={true}
+        xCoordinate={0}
+        yCoordinate={0}
+      />
+    );
+
+    expect(getByText('Start')).toBeVisible();
+    expect(getByRole('img')).toBeVisible();
+
+    expect(container.firstChild).toHaveStyle(`background-color: rgb(155, 155, 155)`);
+    expect(container.firstChild).toHaveStyle(`top: 0px`);
+    expect(container.firstChild).toHaveStyle(`left: 0px`);
+  });
+
+  it('renders correct background-color when node is not on patient path', () => {
+    const { container } = render(
+      <Node
+        icon="test"
+        text="Start"
+        isOnPatientPath={false}
+        xCoordinate={0}
+        yCoordinate={0}
+      />
+    );
+
+    expect(container.firstChild).toHaveStyle(`background-color: rgb(121, 185, 185)`);
+  });
+});
