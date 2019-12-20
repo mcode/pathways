9c27fff658c377414abc8798820c79bcae6b3610
kghoreshi <kghoreshi@mitre.org>
2019-12-20 14:03:16 -0500

    patch for empty resources

--- src/components/PatientRecord/PatientRecord.tsx
+++ src/components/PatientRecord/PatientRecord.tsx
@@ -19,7 +19,11 @@ type PatientRecordProps = {
 };
 
 const getResourceByType = (patientRecord: ReadonlyArray<any>, resourceType: string) => {
-  return patientRecord.filter(resource => resource.resourceType === resourceType);
+  return patientRecord.filter((resource) => {
+      const values = ["id", "meta", "resourceType"]
+      const isEmpty = Object.keys(resource).every(value=>values.includes(value))
+      return resource.resourceType === resourceType && !isEmpty;
+    });
 };
 
 const PatientRecord: FC<PatientRecordProps> = ({ resources }) => {
