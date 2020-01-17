9744a77d3e07f66460b1e0617e027219e01af814
Dan Lee <daniellee@mitre.org>
2020-01-17 16:46:38 -0500

    Refactored generation of cubic command string into function.

--- src/components/Arrow/Arrow.tsx
+++ src/components/Arrow/Arrow.tsx
@@ -16,16 +16,9 @@ const Arrow: FC<ArrowProps> = ({ edge, edgeName, isOnPatientPath, widthOffset })
 
   // Returns path for arrow from edge points
   const createPath = (points: Coordinate[], arrowheadId: string): ReactElement => {
-    const pointsWithOffset = points.map((p, i, arr) => {
-      const x = p.x + widthOffset;
-
-      // Adjust y coordinate of last point for arrowhead
-      const y = i === arr.length - 1 ? p.y - 17.5 : p.y;
-
-      return { x, y };
-    });
-
+    const pointsWithOffset = points.map(p => ({ x: p.x + widthOffset, y: p.y }));
     const length = pointsWithOffset.length;
+    pointsWithOffset[length - 1].y -= 17.5;
     let pathString = `M ${pointsWithOffset[0].x} ${pointsWithOffset[0].y} `;
 
     /**
@@ -35,25 +28,21 @@ const Arrow: FC<ArrowProps> = ({ edge, edgeName, isOnPatientPath, widthOffset })
      * If i % 3 is 2, use all points including the 1st point and reuse every 3rd point
      * We need points to be a multiple of 3 because the Cubic command expects 3 points
      */
-    if (length % 3 === 0) {
-      pathString = pointsWithOffset.reduce((acc, point, i, arr) => {
-        if (i % 3 !== 0) return acc;
-        return `${acc} C ${point.x} ${point.y} ${arr[i + 1].x} ${arr[i + 1].y} ${arr[i + 2].x} 
-        ${arr[i + 2].y}`;
+    const writeCommandString = (remainder: number): string => {
+      return pointsWithOffset.reduce((acc, point, i, arr) => {
+        return i % 3 !== remainder
+          ? acc
+          : `${acc} C ${point.x} ${point.y} ${arr[i + 1].x} ${arr[i + 1].y} ${arr[i + 2].x}
+          ${arr[i + 2].y}`;
       }, pathString);
-    } else if (length % 3 === 1) {
-      pathString = pointsWithOffset.reduce((acc, point, i, arr) => {
-        if (i % 3 !== 1) return acc;
-        return `${acc} C ${point.x} ${point.y} ${arr[i + 1].x} ${arr[i + 1].y} ${arr[i + 2].x} 
-        ${arr[i + 2].y}`;
-      }, pathString);
-    } else {
-      pathString = pointsWithOffset.reduce((acc, point, i, arr) => {
-        if (i !== 0 && i % 3 !== 2) return acc;
-        return `${acc} C ${point.x} ${point.y} ${arr[i + 1].x} ${arr[i + 1].y} ${arr[i + 2].x} 
-        ${arr[i + 2].y}`;
-      }, pathString);
-    }
+    };
+
+    pathString =
+      length % 3 === 0
+        ? writeCommandString(0)
+        : length % 3 === 1
+        ? writeCommandString(1)
+        : writeCommandString(2);
 
     return <path d={pathString} fill="transparent" markerEnd={`url(#${arrowheadId})`} />;
   };
