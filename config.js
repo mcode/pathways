d95d3b89518a7faa7fd722bcfaca8465c6f788da
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-14 15:51:31 +0000

    Deploy mcode/pathways to github.com/mcode/pathways.git:gh-pages

---
+++ config.js
@@ -0,0 +1,6 @@
+CONFIG = {
+  mapper: 'SyntheaToV09', // mapper name from fhir-mapper -- allows mapping of incoming FHIR resources
+  cqlToElmWebserviceUrl: 'https://cql-translation-service-zfk47ib3kq-uc.a.run.app/cql/translator',
+  pathwaysService: 'https://mcode.github.io/pathways/static/pathways/',
+  demoPathwaysService: './static/pathways/'
+};
