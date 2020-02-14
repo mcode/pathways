38f11280780efc371a9d0cce9c0049495b02c067
Reece Adamson <radamson@mitre.org>
2020-02-14 13:22:24 -0500

    remove commented out code

--- src/components/PathwayPopup/PathwayPopup.tsx
+++ src/components/PathwayPopup/PathwayPopup.tsx
@@ -7,23 +7,6 @@ interface PathwayPopupProps {
   Trigger: ReactElement;
   popupPosition: StrictPopupProps['position'];
 }
-// const PathwayPopup: FC<PathwayPopupProps> = ({ name }) => {
-//   return (
-//     <Popup
-//       content={<PopupContent values={['N1', 'N0', 'N+']}></PopupContent>}
-//       position="bottom right"
-//       className={classes.pathwayPopup}
-//       on="click"
-//       pinned
-//       trigger={
-//         <div className={classes.popupWrapper}>
-//           missing data
-//           <FontAwesomeIcon icon="edit" className={nodeClasses.externalLink} />
-//         </div>
-//       }
-//     />
-//   );
-// };
 
 const PathwayPopup: FC<PathwayPopupProps> = ({
   Content,
