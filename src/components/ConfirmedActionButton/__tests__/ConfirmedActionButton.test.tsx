77af7f280c8833210a46b8a3176d1f2ca63d2873
Reece Adamson <radamson@mitre.org>
2020-02-18 11:35:55 -0500

    more tests

---
+++ src/components/ConfirmedActionButton/__tests__/ConfirmedActionButton.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { ConfirmedActionButton } from '../ConfirmedActionButton';
+
+const popupText = 'Are you sure?';
+
+describe('<ConfirmedActionButton />', () => {
+  it('renders the popup when clicked', () => {
+    const { queryByText, getByText } = render(
+      <ConfirmedActionButton size="large" type="accept" />
+    );
+    expect(queryByText(popupText)).toBeNull();
+    fireEvent.click(getByText('Accept'));
+    expect(queryByText(popupText)).not.toBeNull();
+    expect(getByText(popupText)).toBeInTheDocument();
+  });
+});
