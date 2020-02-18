8e6ffca85fc6f910538ef082cf67f65770dbc0fd
Reece Adamson <radamson@mitre.org>
2020-02-18 18:37:05 -0500

    improve test coverage

--- src/components/withConfirmationPopup/__tests__/withConfirmationPopup.test.tsx
+++ src/components/withConfirmationPopup/__tests__/withConfirmationPopup.test.tsx
@@ -28,10 +28,19 @@ describe('withConfirmationPopup', () => {
   });
 
   it('calls the onConfirm callback when the confirmation is accepted', () => {
-    const { getByText, getByTestId } = render(<MockConfirmationWithCallback/>);
+    const { getByText, getByTestId } = render(<MockConfirmationWithCallback />);
     fireEvent.click(getByText(containerText));
     expect(mockCallback).not.toHaveBeenCalled();
     fireEvent.click(getByTestId('accept'));
     expect(mockCallback).toHaveBeenCalled();
+  });
+
+  it('closes the popup when the confirmation is denied', () => {
+    const { getByText, queryByText, getByTestId } = render(<MockWithConfirmation />);
+    expect(queryByText(popupText)).toBeNull();
+    fireEvent.click(getByText(containerText));
+    expect(queryByText(popupText)).not.toBeNull();
+    fireEvent.click(getByTestId('decline'));
+    expect(queryByText(popupText)).toBeNull();
   });
 });
