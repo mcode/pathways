5bddb082abb4704cf949adc4b220429eaf828c4f
Reece Adamson <radamson@mitre.org>
2020-02-18 16:47:33 -0500

    add tests for PathwayPopup

---
+++ src/components/PathwayPopup/__tests__/PathwayPopup.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import PathwayPopup from '../PathwayPopup';
+
+const popupText = 'lorem ipsum';
+const triggerText = 'dolor sit amet';
+
+describe('<PathwayPopup />', () => {
+  it('renders the trigger', () => {
+    const { getByText, queryByText } = render(
+      <PathwayPopup Content={<div>{popupText}</div>} Trigger={<div>{triggerText}</div>} />
+    );
+    expect(queryByText(popupText)).toBeNull();
+    expect(queryByText(triggerText)).not.toBeNull();
+    fireEvent.click(getByText(triggerText));
+    expect(queryByText(popupText)).not.toBeNull();
+    expect(queryByText(triggerText)).not.toBeNull();
+  });
+});
