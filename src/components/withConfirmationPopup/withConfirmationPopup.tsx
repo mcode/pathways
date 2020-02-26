c54f6d1404961b8f0ff5f2be01a51500359c4f6c
Reece Adamson <radamson@mitre.org>
2020-02-26 12:01:09 -0500

    minor type update and naming

--- src/components/withConfirmationPopup/withConfirmationPopup.tsx
+++ src/components/withConfirmationPopup/withConfirmationPopup.tsx
@@ -3,14 +3,16 @@ import styles from './withConfirmationPopup.module.scss';
 import PathwayPopup from 'components/PathwayPopup';
 import ActionButton from 'components/ActionButton';
 
-interface WithConfirmationPopupProps {
+type ConfirmationProps = {
   onConfirm?: () => void;
-}
+};
+
+type WithConfirmationPopupProps<T> = T & ConfirmationProps;
 
 const withConfirmationPopup = <T extends object>(
   WrappedComponent: FC<T>
-): FC<T & WithConfirmationPopupProps> => {
-  const PopupComponent: FC<T & WithConfirmationPopupProps> = ({ onConfirm, ...passed }) => {
+): FC<WithConfirmationPopupProps<T>> => {
+  const PopupComponent: FC<WithConfirmationPopupProps<T>> = ({ onConfirm, ...wrappedProps }) => {
     const [open, setOpen] = useState<boolean>(false);
     // https://github.com/Semantic-Org/Semantic-UI-React/issues/2487
     return (
@@ -20,8 +22,8 @@ const withConfirmationPopup = <T extends object>(
         open={open}
         setOpen={setOpen}
         Trigger={
-          <div className={styles.triggerContainer} {...passed}>
-            <WrappedComponent {...(passed as T)} />
+          <div className={styles.triggerContainer} {...wrappedProps}>
+            <WrappedComponent {...(wrappedProps as T)} />
           </div>
         }
       />
