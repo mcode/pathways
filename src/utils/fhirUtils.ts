415ac46f7eae382342e052bc64c6c189a8799b14
Dan Lee <daniellee@mitre.org>
2020-03-04 20:13:10 -0500

    Throw error if translation for pathway recommended resource not found.

--- src/utils/fhirUtils.ts
+++ src/utils/fhirUtils.ts
@@ -35,7 +35,7 @@ export function translatePathwayRecommendation(
       };
     }
     default: {
-      return {};
+      throw Error(`Translation for ${resourceType} not implemented.`);
     }
   }
 }
