050a3e45079384388ae753a26564c0949e9d1eba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-06 23:04:31 +0000

    Deploy mcode/pathways to github.com/mcode/pathways.git:gh-pages

---
+++ precache-manifest.053a0b3bb1f78b73d0559ba273be3b0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6b6685cd89bc4aa09e56063ce77140",
+    "url": "/pathways/index.html"
+  },
+  {
+    "revision": "debee62244093bd60b75",
+    "url": "/pathways/static/css/main.8206ae46.chunk.css"
+  },
+  {
+    "revision": "7bfb091d91f2ecdf048e",
+    "url": "/pathways/static/js/2.bb1c8ba9.chunk.js"
+  },
+  {
+    "revision": "debee62244093bd60b75",
+    "url": "/pathways/static/js/main.909f9931.chunk.js"
+  },
+  {
+    "revision": "009f86013082c244183a",
+    "url": "/pathways/static/js/runtime-main.d70db0c1.js"
+  },
+  {
+    "revision": "c8fc712a56976a003754a1b64977d73f",
+    "url": "/pathways/static/media/camino-logo-dark.c8fc712a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pathways/precache-manifest.e1f8628b3434e431555ebdbcd3348358.js"
+  "/pathways/precache-manifest.053a0b3bb1f78b73d0559ba273be3b0b.js"
 );
 
 self.addEventListener('message', (event) => {
